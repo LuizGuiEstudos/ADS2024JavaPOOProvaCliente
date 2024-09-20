71dbff0cf7dfb35ebd60e61dc38de1741a3cd2bc
LuizGuiEstudos <157849941+LuizGuiEstudos@users.noreply.github.com>
2024-09-19 22:08:09 -0300

    1.0 - FINAL

---
+++ Prova/atividade1Pessoa/src/entities/Pessoa.java
@@ -0,0 +1,111 @@
+package entities;
+
+import java.util.ArrayList;
+
+public class Pessoa {
+	private static ArrayList<Pessoa> pessoas = new ArrayList<Pessoa>();
+	private static int counter_ID = pessoas.size();
+	
+	public int pes_id;
+	public String nome;
+	public String data_nascimento;
+	
+	private char pes_sexo;
+	private String  pes_mae;
+	private String pes_pai;
+	
+	public String endereco_completo;
+	
+	private String email;
+	private String telefone;
+	
+	public String url_foto;
+	//CONSTRUTORS
+	public Pessoa(String nome, String data_nascimento, char pes_sexo, String pes_mae, String pes_pai,
+			String endereco_completo, String email, String telefone, String url_foto) {
+		this.pes_id = counter_ID++;
+		this.nome = nome;
+		this.data_nascimento = data_nascimento;
+		this.pes_sexo = pes_sexo;
+		this.pes_mae = pes_mae;
+		this.pes_pai = pes_pai;
+		this.endereco_completo = endereco_completo;
+		this.email = email;
+		this.telefone = telefone;
+		this.url_foto = url_foto;
+		
+		addPessoa( this );
+		
+		
+	}
+	public Pessoa() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	//CRUD
+	public static void addPessoa( Pessoa pes ) {
+		pessoas.add(pes);
+	}
+	
+	public static void listarPessoas() 
+	{
+		System.out.println( pessoas.size() );
+		for( Pessoa iPess : pessoas ) 
+		{
+			printPessoa(iPess);
+		}
+	}
+	public static Pessoa buscarTelefone( String tel ) 
+	{
+		
+		//LOOP - BUSCA TELEFONE
+		for( int i = 0; i<=pessoas.size(); i++ ) 
+		{
+			Pessoa IPess = pessoas.get(i);
+			if(  IPess.telefone.equals(tel) ) { //ACHOU
+				return IPess;
+			}
+		}
+		return null; //NÃO ACHOU
+	}
+	
+	public static Pessoa deletarPessoa( int pessID ) 
+	{
+		int pessIndice= encontraIndiceDoID(pessID);
+		if( pessIndice == -1 ) { //<NÃO ACHOU
+			return null;
+		}
+		
+		Pessoa pess = pessoas.get(pessIndice);
+		
+		pessoas.remove(pessIndice); //REMOVIDO
+		return pess;
+		
+	}
+	
+	private static int encontraIndiceDoID( int pessID ) 
+	{
+		for( int i=0; i<pessoas.size(); i++)
+		{
+			Pessoa pess = pessoas.get(i);
+			if( pess.pes_id == pessID )
+			{
+				return i; //ACHOU
+			}
+		}
+		return -1;
+	}
+	
+	public static void printPessoa( Pessoa pess ) 
+	{
+		//PRINT - PESSOA
+		System.out.print(">");
+		System.out.println( "Pessoa ID : " + pess.pes_id );
+		System.out.println( ".nome: "  + pess.nome );
+		//...outros atributos
+		System.out.println( ".telefone: " + pess.telefone );
+	}
+	
+	
+	
+}
---
+++ Prova/atividade1Pessoa/src/main/Main.java
@@ -0,0 +1,143 @@
+package main;
+
+import java.util.Scanner;
+
+import entities.Pessoa;
+
+public class Main {
+
+	private static Scanner scan = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		// LOOP - PANEL
+		int escolha = -1;
+
+		System.out.println(" ### GERENCIAMENTO DE CLIENTES ### ");
+
+		while (escolha != 0) {
+			System.out.println();
+			System.out.println();
+			// ENTRADA
+			System.out.print("-Escolha uma ação: ");
+			System.out.println("1-Cadastrar, 2-Listar todos, 3-Buscar por telefone, \n"
+					+ " 4-Deletar pessoa por ID, 0-Sair ");
+			System.out.print("< ");
+
+			escolha = scan.nextInt();
+
+			// TOMAR ACAO
+			switch (escolha) {
+			case 1: addPessoa();
+				break;
+			case 2: listarPessoas();
+				break;
+			case 3: buscarPessoa();
+				break;
+			case 4: deletarPessoa();
+				break;
+			case 0: System.out.println( ">saindo..." ); 
+				break;
+			default: System.out.println( ">Ação inválida!!!" );
+			}
+
+		}
+		
+		System.out.println( " FIM - GERENCIAMENTO DE CLIENTES " );
+
+	}
+	
+	private static void deletarPessoa() 
+	{
+		int scnID;
+		
+		System.out.println( " -- DELETAR PESSOA -- " );
+		System.out.println( "-Digite o id da pessoa a ser deletada:" );
+		System.out.print( "< " );
+		
+		scnID = scan.nextInt();
+		
+		Pessoa pessoaDeletada = Pessoa.deletarPessoa(scnID);
+		
+		if( pessoaDeletada == null  ) //<<NÃO ENCOTROU 
+		{
+			System.out.println( " ID não existente! " );
+			return;
+		}
+		System.out.println( " >A seguinte pessoa foi deletada: " );
+		Pessoa.printPessoa(pessoaDeletada);
+		
+	}
+
+	private static void listarPessoas() {
+		System.out.println( " -- PRINTANDO PESSOAS -- " );
+		Pessoa.listarPessoas();
+	}
+	
+	private static void buscarPessoa() {
+		String pTel;
+		Pessoa pBuscada;
+		
+		System.out.println( " -- BUSCAR PESSOA POR TELEFONE -- " );
+		System.out.println( "-Digite o TELEFONE da pessoa para busca-la" );
+		System.out.print("< ");
+		pTel = scan.next();
+		
+		pBuscada = Pessoa.buscarTelefone(pTel);
+		
+		System.out.println( ">O TELEFONE " + pTel + " pertence a seguinte pessoa: " );
+		Pessoa.printPessoa(pBuscada);
+		
+	}
+	
+	private static void addPessoa() {
+		// ENTRADA - PESSOA atributos
+		Pessoa nPessoa;
+		
+		System.out.println( " -- ADICIONAR PESSOA COM ATRIBUTOS --" );
+
+		System.out.println("-Digite o nome da pessoa: ");
+		System.out.print("< ");
+		String pesNome = scan.next();
+
+		System.out.println("-Digite a data de nascimento: ");
+		System.out.print("< ");
+		String pesDataNascimento = scan.next();
+
+		System.out.println("-Digite o sexo: ");
+		System.out.print("< ");
+		char pesSexo = scan.next().charAt(0);
+
+		System.out.println("-Digite o nome da mãe: ");
+		System.out.print("< ");
+		String pesMae = scan.next();
+
+		System.out.println("-Digite o nome da pai: ");
+		System.out.print("< ");
+		String pesPai = scan.next();
+
+		System.out.println("-Digite o endereço completo: ");
+		System.out.print("< ");
+		String pesEndereco = scan.next();
+
+		System.out.println("-Digite o email: ");
+		System.out.print("< ");
+		String pesEmail = scan.next();
+
+		System.out.println("-Digite o TELEFONE: ");
+		System.out.print("< ");
+		String pesTel = scan.next();
+
+		System.out.println("-Digite a URL da foto: ");
+		System.out.print("< ");
+		String pesFoto = scan.next();
+
+		nPessoa = new Pessoa(pesNome, pesDataNascimento, pesSexo, pesMae, pesPai, pesEndereco, pesEmail, pesTel,
+				pesFoto);
+
+		System.out.println(">A seguinte pessoa foi cadastrada: ");
+		Pessoa.printPessoa(nPessoa);
+
+	}
+
+}
